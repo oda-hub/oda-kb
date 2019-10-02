f1d5e8214a79b538d0ff8dfd681f4c6c5635b487
Volodymyr <vladimir.savchenko@gmail.com>
2019-10-02 11:10:58 +0200

    basic query

--- odasparql.py
+++ odasparql.py
@@ -37,10 +37,38 @@ def get_planning():
 
 
     print(r)
-
     print(r.text)
 
 
+def query(query, prefixes=None, debug=True):
+    default_prefixes=[
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>",
+        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+        "prefix dda: <http://ddahub.io/ontology/analysis#>",
+    ]
+
+    _prefixes = default_prefixes
+
+    if prefixes is not None:
+        _prefixes += prefixes
+        
+
+    _query = "\n".join(_prefixes) + "\n\n" + query
+
+    if debug:
+        print("sending:\n", _query)
+
+    r=requests.post('http://fuseki.internal.odahub.io/dataanalysis/query',
+                   params=dict(query=_query)
+                )
+
+
+    print(r)
+    print(r.text)
+
 #create()
-get_planning()
+
+if __name__ == "__main__":
+    get_planning()
 
