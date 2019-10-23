df2c051c73a3d5788f64df9d94addbce06f11757
Volodymyr <vladimir.savchenko@gmail.com>
2019-10-23 18:46:41 +0200

    added my email

--- odakb/sparql.py
+++ odakb/sparql.py
@@ -2,6 +2,8 @@ import requests
 import copy
 import os
 
+import time
+
 
 default_prefixes=[
     "PREFIX owl: <http://www.w3.org/2002/07/owl#>",
@@ -15,6 +17,30 @@ default_prefixes=[
 ]
 
 query_stats = None
+
+def start_stats_collection():
+    query_stats=[]
+
+def stop_stats_collection():
+    query_stats=None
+
+def note_stats(**kwargs):
+    if query_stats is not None:
+        query_stats.append(kwargs)
+
+def report_stats():
+    print(query_stats)
+
+    if query_stats is not None:
+        summary=dict(
+            queries=query_stats,
+            spent_seconds=sum([s['spent_seconds'] for s in query_stats]),
+            query_size=sum([s['spent_seconds'] for s in query_stats]),
+        )
+
+        return summary
+
+start_stats_collection()
 
 class SPARQLException(Exception):
     pass
@@ -36,6 +62,8 @@ def execute_sparql(data, endpoint, debug, invalid_raise):
     else:
         auth=None
 
+    t0=time.time()
+
     if endpoint == "update":    
         r=requests.post('http://fuseki.internal.odahub.io/dataanalysis/'+endpoint,
                         data=data,
@@ -45,6 +73,8 @@ def execute_sparql(data, endpoint, debug, invalid_raise):
         r=requests.post('http://fuseki.internal.odahub.io/dataanalysis/'+endpoint,
                        params=dict(query=data)
                     )
+
+    note_stats(spent_seconds=time.time()-t0, query_size=len(data))
     
     if debug:
         print(r)
