bd2b220e80457e567bab67b739cb49d29482672f
Volodymyr <vladimir.savchenko@gmail.com>
2019-11-09 10:10:40 +0100

    allow to fetch remote workflows

--- odakb/evaluator.py
+++ odakb/evaluator.py
@@ -3,6 +3,9 @@ import io
 import glob
 import yaml
 import hashlib
+import subprocess
+import re
+import odakb.sparql as sp
 
 import nb2workflow.nbadapter as nba
 
@@ -18,47 +21,86 @@ def build_local_context():
     return context
 
 
+def evaluate_local(query, kwargs, context):
+    query_alias = query.replace("http://","").replace("/",".") 
+
+    print("will evaluate with local context", context[query])
+
+    s=io.StringIO()
+    yaml.dump(kwargs, s)
+    qc = hashlib.sha256(s.getvalue().encode()).hexdigest()[:8]
+
+    fn = "data/{}-{}.yaml".format(query_alias, qc)
+
+    if os.path.exists(fn):
+        d=yaml.load(open(fn))
+
+    else:
+        nbnames = glob.glob(context[query])
+        assert len(nbnames) == 1
+        nbname = nbnames[0]
+    
+        d = nba.nbrun(nbname, kwargs)
+
+        for k, v in d.items():
+            try:
+                d[k] = yaml.load(v)
+            except: pass
+
+        try:
+            os.makedirs("data")
+        except:
+            pass
+    
+        yaml.dump(d, open(fn, "w"))
+
+    return d
+
+def resolve_callable(query):
+    r=sp.select(None, "<%s> oda:callableKind ?kind ."%query)
+    print(r)
+    
+    if r['results']['bindings'] == []:
+        callable_kind="http://odahub.io/callable/notebook"
+
+    r=sp.select(None, "<%s> oda:location ?kind ."%query)
+    
+    print(r)
+
+    locations = r['results']['bindings']
+    assert len(locations) == 1
+    location= locations[0]
+    
+    return callable_kind, location['kind']['value']
+
+def fetch_location(location, query):
+    local_location = os.path.join("code",
+                                  re.sub("^http", "", 
+                                  re.sub("[:/]", "", query))
+                                )
+
+    try:
+        subprocess.check_call(["git", "clone", location, local_location])
+    except:
+        subprocess.check_call(["git", "pull"],cwd=local_location)
+
 def evaluate(query, *subqueries, **kwargs):
+    callable_kind, location = resolve_callable(query)       
+
+    if location.startswith("https"):
+        fetch_location(location, query)
+
     context = build_local_context()
 
     # construct kwargs from sub queries
 
     if query.startswith("http://"):
+
+
         if query in context:
-            query_alias = query.replace("http://","").replace("/",".") 
-        
-            print("will evaluate with local context", context[query])
+            d = evaluate_local(query, kwargs, context)
 
-            s=io.StringIO()
-            yaml.dump(kwargs, s)
-            qc = hashlib.sha256(s.getvalue().encode()).hexdigest()[:8]
-
-            fn = "data/{}-{}.yaml".format(query_alias, qc)
-
-            if os.path.exists(fn):
-                d=yaml.load(open(fn))
-
-            else:
-                nbnames = glob.glob(context[query])
-                assert len(nbnames) == 1
-                nbname = nbnames[0]
-            
-                d = nba.nbrun(nbname, kwargs)
-
-                for k, v in d.items():
-                    try:
-                        d[k] = yaml.load(v)
-                    except: pass
-
-                try:
-                    os.makedirs("data")
-                except:
-                    pass
-            
-                yaml.dump(d, open(fn, "w"))
-
-            return d
-
+    return d
             
 
     
