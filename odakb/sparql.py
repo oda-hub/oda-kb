0ae11dc1d2f0b2c5a03487f420d5caa7724a1f9c
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2019-12-27 11:33:52 +0100

    fix version

--- odakb/sparql.py
+++ odakb/sparql.py
@@ -11,6 +11,7 @@ import click
 
 
 default_prefixes=[
+    "PREFIX an: <http://ddahub.io/ontology/analysis#>",
     "PREFIX owl: <http://www.w3.org/2002/07/owl#>",
     "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
