e0cd1621b9ea8ad06af53cb2c621665ab5fbc292
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-15 12:33:44 +0200

    Bump version: 0.5.0 → 0.5.1-dev0

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="oda-sparql",
       long_description=open('README.md').read(),
-      version='0.5.0',
+      version='0.5.1-dev0',
       include_package_data=True,
       setup_requires=setup_requires)
