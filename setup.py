e9a80673f04aa0314445a1b088a8315d5bb129f2
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2020-08-17 18:44:12 +0200

    less pythons better

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import ast
 import sys
 
-setup_requires = ['setuptools >= 30.3.0', 'setuptools-git-version']
+setup_requires = ['setuptools >= 30.3.0']
 
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
     setup_requires.append('pytest-runner')
