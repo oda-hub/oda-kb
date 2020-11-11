4587e9fa54c267d858d78cefdb08beec8b660f61
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-11 14:03:27 +0100

    Bump version: 0.6.26-dev0 → 0.6.26

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="oda-sparql",
 #      long_description=open('README.md').read(),
-      version='0.6.26-dev0',
+      version='0.6.26',
       include_package_data=True,
       setup_requires=setup_requires)
