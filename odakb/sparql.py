1f22b40462493d0ba6abbca0b0bacb01fea48a11
burnout87 <gabriele.barni@gmail.com>
2025-09-03 16:32:49 +0200

    urls ontology_mirror

--- odakb/sparql.py
+++ odakb/sparql.py
@@ -112,7 +112,7 @@ def load_defaults(default_prefixes, default_graphs):
     
 
     for ontology_mirror in [
-                    "https://volodymyrss.github.io/",
+                    "https://www.astro.unige.ch/~savchenk/volodymyrss.github.io/",
                     "http://ontology.odahub.io/",
                 ]:
         try:
